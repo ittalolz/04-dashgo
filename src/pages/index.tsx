3f3bb30cfaac3a848d8af321e827d071b4777f0d
Ittalo Steltter <ittalolz@gmail.com>
2022-04-29 17:05:15 -0400

    feat: use form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form/Input";
 
+type SignInFormatData = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn() {
+  const { register, handleSubmit } = useForm();
+
+  const handelSignIn: SubmitHandler<SignInFormatData> = (value) => {
+    console.log(value);
+  };
+
   return (
     <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
       <Flex
@@ -12,10 +24,21 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handelSignIn)}
       >
         <Stack spacing="4">
-          <Input type="email" name="email" label="E-mail" />
-          <Input type="password" name="password" label="Senha" />
+          <Input
+            type="email"
+            name="email"
+            label="E-mail"
+            {...register("email")}
+          />
+          <Input
+            type="password"
+            name="password"
+            label="Senha"
+            {...register("password")}
+          />
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" size="lg">
